087d304e76b9159e6b707b5976accffc1245c3cc
Adam Wolff <adam@sparko.localdomain>
2022-12-08 17:06:31 -0800

    Finally

--- day08/main.py
+++ day08/main.py
@@ -1,29 +1,48 @@
+from functools import reduce
+
 def part1(inp):
+    print(f"Part 1: {sum(process_grid(inp, count_visible))}")
+
+def part2(inp):
+    print(f"Part 2: {max(process_grid(inp, score_scenicness))}")
+
+def process_grid(inp, f):
     grid = list(map(lambda line: list(map(int, line)), inp.splitlines()))
-    # print(grid)
-    count = 0
     grid_width = len(grid[0])
     grid_height = len(grid)
     for y in range(0, grid_height):
         for x in range(0, grid_width):
-            # print(f"check {x},{y} {grid[y][x]}")
-            if is_visible(grid, x, y):
-                count += 1
-    print(f"Trees visible: {count}")
+            row = grid[y]
+            col = [r[x] for r in grid]
+            l, r = [*reversed(row[:x])], row[x+1:]
+            t, b = [*reversed(col[:y])], col[y+1:]
+            height = grid[y][x]
+            result = f(height, [l, r, t, b])
+            # if result > 10000:
+            # if result:
+            #     print(f"**{x},{y} [{height}] {result}**")
+            yield result
 
-def is_visible(grid, x, y):
-    tree_height = grid[y][x]
-    def is_below(l):
-        if not len(l):
-            return True
+def count_visible(height, vecs):
+    for vec in vecs:
+        if len(vec) == 0 or max(vec) < height:
+            return 1
+    return 0
 
-        # print(f"   {tree_height} {l}")
-        return max(l) < tree_height 
+def score_scenicness(height, vecs):
+    def count_vec(vec):
+        count = 0
+        for tree in vec:
+            count += 1
+            if not height > tree:
+                break
+        return count
 
-    row = grid[y]
-    col = [r[x] for r in grid]
-    # print(f" {row} {col}")
-    return is_below(row[:x]) or is_below(row[x+1:]) or is_below(col[:y]) or is_below(col[y+1:])
+    counted = [*map(count_vec, vecs)]
+    product = reduce((lambda x, y: x * y), counted)
+    return product
+
+
 
 
 def get_sample():
@@ -34,5 +53,7 @@ def get_sample():
 35390
 """
 
-# part1(get_sample())
+part1(get_sample())
 part1(open("input.txt").read())
+part2(get_sample())
+part2(open("input.txt").read())
